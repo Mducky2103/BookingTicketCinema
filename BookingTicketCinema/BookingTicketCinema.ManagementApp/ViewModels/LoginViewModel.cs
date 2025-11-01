7f5be4029f663bd6e87aa0065392774e21b2363c
Ng Duc Minh <ducminh211103@gmail.com>
2025-11-01 17:43:49 +0700

    Add Admin/Staff Client & Login

---
+++ BookingTicketCinema/BookingTicketCinema.ManagementApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingTicketCinema.ManagementApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
