962d486f570e5079871e149d16eb1207704c24ea
Mira1311 <angryxious2003@gmail.com>
2025-11-03 13:00:57 +0700

    validation update

--- BookingTicketCinema/BookingTicketCinema/DTO/UpdateUserDto.cs
+++ BookingTicketCinema/BookingTicketCinema/DTO/UpdateUserDto.cs
@@ -1,11 +1,19 @@
 ﻿using Org.BouncyCastle.Utilities;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookingTicketCinema.DTO
 {
     public class UpdateUserDto
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; } = default!;
+        [Required(ErrorMessage = "Full name is required")]
+        [MinLength(2, ErrorMessage = "Full name must be at least 2 characters long")]
         public string FullName { get; set; } = default!;
+        [Required(ErrorMessage = "Invalid phone number")]
+        [RegularExpression(@"^\d{10,11}$",
+            ErrorMessage = "Phone number must contain 10 to 11 digits")]
         public string? PhoneNumber { get; set; }
     }
 }
--- BookingTicketCinema/BookingTicketCinema/DTO/UserRegistrationModel.cs
+++ BookingTicketCinema/BookingTicketCinema/DTO/UserRegistrationModel.cs
@@ -1,12 +1,25 @@
-﻿namespace BookingTicketCinema.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingTicketCinema.DTO
 {
     public class UserRegistrationModel
     {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
         public string Email { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[\W_]).{6,}$",
+            ErrorMessage = "Password must contain at least one uppercase letter and one special character.")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Full Name is required.")]
         public string FullName { get; set; }
+        [Required(ErrorMessage = "Role is required.")]
         public string Role { get; set; }
+        [Required(ErrorMessage = "Gender is required.")]
         public string Gender { get; set; }
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
     }
 }
